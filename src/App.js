a039b74358a7b3de1b9b7a09cd296db7b9867559
hamzaa25 <hamza.polat@outlook.de>
2021-12-30 16:29:26 +0100

    merge

--- src/App.js
+++ src/App.js
@@ -1,70 +1,76 @@
-import React, { useState } from "react";
-import ImageMarker  from 'react-image-marker';
-import Marker  from 'react-image-marker';
-
-// const [markers, setMarkers] = useState('');
-
-// const [markers, setMarkers] =
-//     useState <
-//     Array <
-//     Marker >>
-//         [
-//             {
-//                 top: 10,
-//                 left: 50, //50% of the image relative size from left
-//             },
-//         ];
+import React, { useState, Component} from "react";
+import './App.css';
+import URLimage from './components/URLimage'
+import ImageMarker, { Marker, MarkerComponentProps } from 'react-image-marker';
 
 
-  // const CustomMarker = (props: MarkerComponentProps) => {
-  //         return (
-  //             <p className="custom-marker">My custom marker - {props.itemNumber}</p>
-  //         );
-  //     };  
-
-class App extends React.Component {
-  
-  constructor(props){
-    super(props);
-    this.state = {
-      value: ''
-    };
-    this.onChange = this.onChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  
-  onChange(event) {
-    this.setState({value: event.target.value});
-  }
-
-  handleSubmit(event) {
-    event.preventDefault();
-  }
+class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        <h1 style={{ margin: '10px', }}> Simple Annotation Tree</h1>
-        <h2 style={{ margin: '10px' }} >1. Zuerst fügen Sie die URL ein und klicken Sie auf den Kopf der Personen</h2>
-        <form onSubmit={this.handleSubmit}>
-        <label style={{ margin: '10px' }}>
-          <a> Fügen Sie die URL hier ein:  </a>
-          <input style={{ padding: '5px', width: '50pc'}} type="text" value={this.state.value} onChange={this.onChange} />
-        </label>
-      </form>
-        <div style={{ margin: '10px' }}> 
-        {/* <ImageMarker
-             src={this.state.value}
-             markers={markers}
-             onAddMarker={(marker: Marker) => setMarkers([...markers, marker])}
-             markerComponent={CustomMarker}
-        /> */}
-          <img style={{ width: '90%', height: '90%' }} src={this.state.value} alt={this.state.value}/>
-        </div>
+      <div>
+        <Form />
+        <Img/>
       </div>
+ 
     );
   }
+}
+
+function Form() {
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(useState.markers);
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <button type="submit">Submit</button>
+    </form>
+  );
+}
+
+function Img() {
+  const CustomMarker = (props: MarkerComponentProps) => {
+    return (
+      <div style={{width: '40px', height: '40px', borderRadius: '25px', background: 'red'}} className="custom-marker"></div>
+    );
+  };
+
+  const [markers, setMarkers] = useState([{}]);
+  const [name, setURL] = useState("");
+
+  const handleSubmit = (evt) => {
+    evt.preventDefault();
+  }
+
+return (
+    <div className="App">
+     <h1 style={{ margin: '10px', }}> Simple Annotation Tree</h1>
+      <h2 style={{ margin: '10px' }} >1. Zuerst fügen Sie die URL ein und klicken Sie auf den Kopf der Personen</h2>
+
+      <form onSubmit={handleSubmit}>
+      <label style={{ margin: '10px' }}>
+        <a> Fügen Sie die URL hier ein:  </a>
+        <input style={{ padding: '5px', width: '50pc'}} type="text" value={name} onChange={e => setURL(e.target.value)}/>
+      </label>
+    </form>
+
+    <h2 style={{ margin: '10px' }} >2. Klicken Sie auf jeden Kopf den Sie sehen! </h2>
+    <ImageMarker
+     src={name}
+     alt= "Hier erscheint das Bild!"
+      markers={markers}
+      coordinate={() => console.log('TEST')}
+      onAddMarker={(marker: Marker) => setMarkers([...markers, marker])}
+      markerComponent={CustomMarker}
+      style={{
+        cursor: "pointer"
+      }}
+    />
+    <div className="markerInfo"> Das ist die Markierung!</div> 
+  </div>
+  );
 }
 
 export default App;
